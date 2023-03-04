781ad62ce1b7528a3a53a30a7141d38fe0fe0604
David Ahmadov <ahmedavid@gmail.com>
2023-03-04 14:14:55 +0400

    dd

--- main.tf
+++ main.tf
@@ -16,37 +16,40 @@ data "azurerm_resource_group" "existing_rg" {
   name = "ODL-azure-878220"
 }
 
-resource "azurerm_container_group" "tfcg_test" {
-  name = "weatherapi"
-  location = azurerm_resource_group.existing_rg.location
-  resource_group_name = azurerm_resource_group.existing_rg.name
-
-  ip_address_type = public
-  dns_name_label = "ahmedavidwa"
-  os_type = "Linux"
-
-  container {
-    name = "weatherapi"
-    image = "ahmedavid/webapi"
-    cpu = "1"
-    memory = "1"
-
-    ports {
-      port = 80
-      protocol = "TCP"
-    }
-  }
+output "resource_group_location" {
+  value = data.azurerm_resource_group.existing_rg.location
 }
+# resource "azurerm_container_group" "tfcg_test" {
+#   name = "weatherapi"
+#   location = azurerm_resource_group.existing_rg.location
+#   resource_group_name = azurerm_resource_group.existing_rg.name
 
-# resource "azurerm_resource_group" "tf_test" {
-#   name = "ODL-azure-878220"
-#   location = "East US"
-# }
+#   ip_address_type = public
+#   dns_name_label = "ahmedavidwa"
+#   os_type = "Linux"
 
-# resource "azurerm_resource_group" "tf_test" {
-#   name     = data.azurerm_resource_group.existing_rg.name
-#   location = data.azurerm_resource_group.existing_rg.location
-#   lifecycle {
-#     prevent_destroy = true
+#   container {
+#     name = "weatherapi"
+#     image = "ahmedavid/webapi"
+#     cpu = "1"
+#     memory = "1"
+
+#     ports {
+#       port = 80
+#       protocol = "TCP"
+#     }
 #   }
 # }
+
+# # resource "azurerm_resource_group" "tf_test" {
+# #   name = "ODL-azure-878220"
+# #   location = "East US"
+# # }
+
+# # resource "azurerm_resource_group" "tf_test" {
+# #   name     = data.azurerm_resource_group.existing_rg.name
+# #   location = data.azurerm_resource_group.existing_rg.location
+# #   lifecycle {
+# #     prevent_destroy = true
+# #   }
+# # }
