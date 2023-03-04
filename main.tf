1a28343be4f314bc301aa5f9252edbd6debdff6c
David Ahmadov <ahmedavid@gmail.com>
2023-03-04 14:04:10 +0400

    tf

--- main.tf
+++ main.tf
@@ -16,15 +16,37 @@ data "azurerm_resource_group" "existing_rg" {
   name = "ODL-azure-878220"
 }
 
+resource "azure_container_group" "tfcg_test" {
+  name = "weatherapi"
+  location = azurerm_resource_group.existing_rg.location
+  resource_group_name = azurerm_resource_group.existing_rg.name
+
+  ip_address_type = public
+  dns_name_label = "ahmedavidwa"
+  os_type = "Linux"
+
+  container {
+    name = "weatherapi"
+    image = "ahmedavid/webapi"
+    cpu = "1"
+    memory = "1"
+
+    ports {
+      port = 80
+      protocol = "TCP"
+    }
+  }
+}
+
 # resource "azurerm_resource_group" "tf_test" {
 #   name = "ODL-azure-878220"
 #   location = "East US"
 # }
 
-resource "azurerm_resource_group" "tf_test" {
-  name     = data.azurerm_resource_group.existing_rg.name
-  location = data.azurerm_resource_group.existing_rg.location
-  lifecycle {
-    prevent_destroy = true
-  }
-}
+# resource "azurerm_resource_group" "tf_test" {
+#   name     = data.azurerm_resource_group.existing_rg.name
+#   location = data.azurerm_resource_group.existing_rg.location
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
