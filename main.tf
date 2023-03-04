3ecdae9fdac03cc62c4cf9b1563626d137751ce1
David Ahmadov <ahmedavid@gmail.com>
2023-03-04 13:53:00 +0400

    tf 2

--- main.tf
+++ main.tf
@@ -12,7 +12,16 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "tf_test" {
+data "azurerm_resource_group" "existing_rg" {
   name = "ODL-azure-878220"
-  location = "East US"
+}
+
+# resource "azurerm_resource_group" "tf_test" {
+#   name = "ODL-azure-878220"
+#   location = "East US"
+# }
+
+resource "azurerm_resource_group" "tf_test" {
+  name     = data.azurerm_resource_group.existing_rg.name
+  location = data.azurerm_resource_group.existing_rg.location
 }
